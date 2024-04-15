8d465cbb61960bcb280ae53c779bdab99c20e41a
bertreux <115898406+bertreux@users.noreply.github.com>
2024-04-15 14:56:05 +0200

    test action avec bon pytest

--- test_job.py
+++ test_job.py
@@ -3,4 +3,4 @@ from job import calculate_average
 def test_calculate_average():
     assert calculate_average([1, 2, 3, 4, 5]) == 3.0
     assert calculate_average([10, 20, 30]) == 20.0
-    assert calculate_average([]) == 1
+    assert calculate_average([]) == 0
